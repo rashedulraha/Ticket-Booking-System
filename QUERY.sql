e1a29820575374a14ae1c75c223062e71b568047
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-06-16 18:18:36 +0600

    Query 2: Search for all users whose names start with 'Tanvir' or contain 'Haque'

--- QUERY.sql
+++ QUERY.sql
@@ -3,3 +3,10 @@ SELECT match_id, fixture, base_ticket_price
 FROM Matches 
 WHERE tournament_category = 'Champions League' 
   AND match_status = 'Available';
+
+
+  -- Query 2: Search for all users whose names start with 'Tanvir' or contain 'Haque' 
+SELECT user_id, full_name, email 
+FROM Users 
+WHERE full_name ILIKE 'Tanvir%' 
+   OR full_name ILIKE '%Haque%';
