e406ab845d34aef094e18589db9327847fd33f96
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-06-16 18:15:04 +0600

    Insertion for Matches

--- SCHEMA.sql
+++ SCHEMA.sql
@@ -35,3 +35,12 @@ INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
 (2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
 (4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+
+
+-- Insertion for Matches
+INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
