f68232b07ae5c95ba99f65a77f51f7361e61ae3e
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-06-16 18:15:28 +0600

    Insertion for Bookings

--- SCHEMA.sql
+++ SCHEMA.sql
@@ -44,3 +44,12 @@ INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, 
 (103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
 (104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
 (105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
+
+
+-- Insertion for Bookings
+INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150.00),
+(502, 1, 102, 'B-04', 'Confirmed', 120.00),
+(503, 2, 101, 'A-13', 'Confirmed', 150.00),
+(504, 2, 101, NULL, NULL, 150.00),
+(505, 3, 102, 'C-20', 'Pending', 120.00);
