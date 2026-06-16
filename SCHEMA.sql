48b8ee3d5c62e017f9e03918c0cb332131d37f0f
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-06-16 18:13:28 +0600

    Create Matches Table

--- SCHEMA.sql
+++ SCHEMA.sql
@@ -6,3 +6,13 @@ CREATE TABLE Users (
     role VARCHAR(50) NOT NULL CHECK (role IN ('Ticket Manager', 'Football Fan')),
     phone_number VARCHAR(20)
 );
+
+
+-- 2. Create Matches Table
+CREATE TABLE Matches (
+    match_id SERIAL PRIMARY KEY,
+    fixture VARCHAR(150) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price DECIMAL(10, 2) NOT NULL CHECK (base_ticket_price >= 0),
+    match_status VARCHAR(50) NOT NULL CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+);
