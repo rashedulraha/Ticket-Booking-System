aa4234661a6116fd2ef0c2991d6daaee839395e5
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-06-16 18:19:04 +0600

    Query 3: Retrieve all booking records where payment status is NULL

--- QUERY.sql
+++ QUERY.sql
@@ -10,3 +10,11 @@ SELECT user_id, full_name, email
 FROM Users 
 WHERE full_name ILIKE 'Tanvir%' 
    OR full_name ILIKE '%Haque%';
+
+
+
+   -- Query 3: Retrieve all booking records where payment status is NULL
+SELECT booking_id, user_id, match_id, 
+       COALESCE(payment_status, 'Action Required') AS systematic_status 
+FROM Bookings 
+WHERE payment_status IS NULL;
